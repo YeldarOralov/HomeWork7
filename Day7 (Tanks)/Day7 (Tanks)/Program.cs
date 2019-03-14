9f6d1378bce384678bc745b355249943d6a10b9a
Eldar Oralov <eldar.oralov@gmail.com>
2019-03-15 02:22:15 +0600

    Second commit

---
+++ Day7 (Tanks)/Day7 (Tanks)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank tankT34One = new Tank("Авалон");
+            Tank tankT34Two = new Tank("Мечта");
+            Tank tankT34Three = new Tank("Разрушитель");
+            Tank tankT34Four = new Tank("Деструктор");
+            Tank tankT34Five = new Tank("Радуга");
+            Tank tankPanteraOne = new Tank("ПиуПиу");
+            Tank tankPanteraTwo = new Tank("Лидер");
+            Tank tankPanteraThree = new Tank("Ласточка");
+            Tank tankPanteraFour = new Tank("Антанта");
+            Tank tankPanteraFive = new Tank("Медведь");
+
+            tankT34One.TankInfo();            
+            tankPanteraOne.TankInfo();
+            var fight = tankT34One * tankPanteraOne;
+
+            tankT34Two.TankInfo();
+            tankPanteraTwo.TankInfo();
+            fight = tankT34Two * tankPanteraTwo;
+
+            tankT34Three.TankInfo();
+            tankPanteraThree.TankInfo();
+            fight = tankT34Three * tankPanteraThree;
+
+            tankT34Four.TankInfo();
+            tankPanteraFour.TankInfo();
+            fight = tankT34Four * tankPanteraFour;
+
+            tankT34Five.TankInfo();
+            tankPanteraFive.TankInfo();
+            fight = tankT34Five * tankPanteraFive;
+
+            
+            Console.Read();
+        }
+    }
+}
---
+++ Day7 (Tanks)/MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        private string name;
+        private int levelOfAmmunition;
+        private int levelOfArmor;
+        private int levelOfManeuverability;
+        public string Name { get { return name; } set { name = value; } }
+        public int LevelOfAmmunition { get { return levelOfAmmunition; } set { levelOfAmmunition = value; } }
+        public int LevelOfArmor { get { return levelOfArmor; } set { levelOfArmor = value; } }
+        public int LevelOfManeuverability { get { return levelOfManeuverability; } set { levelOfManeuverability = value; } }
+        static Random random = new Random();
+        public Tank(string _name)
+        {           
+            name = _name;
+            levelOfAmmunition = random.Next(0, 100);            
+            levelOfArmor = random.Next(0, 100);
+            levelOfManeuverability = random.Next(0, 100);
+        }
+        public void TankInfo()
+        {
+            Console.WriteLine(
+                $"Параметры танка {name}\n" +
+                $"Уровень боекомплекта: {levelOfAmmunition}\n" +
+                $"Уровень брони: {levelOfArmor}\n" +
+                $"Уровень маневренности: {levelOfManeuverability}\n"
+                );
+        }
+        public static Tank operator *(Tank firstTank, Tank secondTank)
+        {
+            int firstWin = 0;
+            int secondWin = 0;
+            Console.WriteLine($"Сражение танка {firstTank.name} и {secondTank.name}");
+            if (firstTank.levelOfAmmunition > secondTank.levelOfAmmunition)
+            {
+                firstWin += 1;
+            }
+            else
+            {
+                secondWin += 1;
+            }
+            if (firstTank.levelOfArmor > secondTank.levelOfArmor)
+            {
+                firstWin += 1;
+            }
+            else
+            {
+                secondWin += 1;
+            }
+            if (firstTank.levelOfManeuverability > secondTank.levelOfManeuverability)
+            {
+                firstWin += 1;
+            }
+            else
+            {
+                secondWin += 1;
+            }
+            if (firstWin > secondWin)
+            {
+                Console.WriteLine($"Побеждает танк {firstTank.name}\n");
+                return firstTank;
+            }
+            else
+            {
+                Console.WriteLine($"Побеждает танк {secondTank.name}\n");
+                return secondTank;
+            }
+        }
+    }
+}
